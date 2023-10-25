d817e523221c5ec3edec511c5d64941a46359484
Kauang502 <134523211+Kauang502@users.noreply.github.com>
2023-10-25 12:08:50 -0300

    ooooooooooooooooooo

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function tocaSomPom(){
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 function tocaSomClap(){
-    document.querySelector('#Som_tecla_clap').play();
+    document.querySelector('#som_tecla_clap').play();
 
 }
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
